7261a87808b4af6d3591a1b9995ba23ef7eb1e44
Bruno Henrique <brunohenriquex00@gmail.com>
2023-09-02 15:37:59 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int numero1 = 10;
+        int numero2 = 5;
+        String nome = "João";
+
+        
+        int soma = numero1 + numero2;
+        int subtracao = numero1 - numero2;
+        int multiplicacao = numero1 * numero2;
+        int divisao = numero1 / numero2;
+
+        // Imprimir os resultados das operações
+        System.out.println("Número 1: " + numero1);
+        System.out.println("Número 2: " + numero2);
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+
+        
+        System.out.println("Olá, " + nome + "! Bem-vindo ao nosso programa!");
+    }
+
+}
