a4de091886ba1c0e46c9fdfb106ae2947e88e332
Bruno Henrique <brunohenriquex00@gmail.com>
2023-10-26 19:07:57 -0300

    adicionando pasta contendo o backend em spring do projeto para um mercado

---
+++ mkshop/src/main/java/com/mkshop/mkshop/MkshopApplication.java
@@ -0,0 +1,13 @@
+package com.mkshop.mkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkshopApplication.class, args);
+	}
+
+}
---
+++ mkshop/src/test/java/com/mkshop/mkshop/MkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkshop.mkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
