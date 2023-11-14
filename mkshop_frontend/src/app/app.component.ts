6842c22a5944e149be66ee2d0f4e7a26da73a436
Bruno Henrique <brunohenriquex00@gmail.com>
2023-11-14 13:56:08 -0300

    instalando e configurando lib primeng

--- mkshop_frontend/src/app/app.component.ts
+++ mkshop_frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  date: Date | undefined;
+
   title = 'mkshop_frontend';
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
