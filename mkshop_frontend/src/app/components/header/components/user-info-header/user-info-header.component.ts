0b2ca57707f26aeaddab7b209a3cd48d37f5d250
Bruno Henrique <brunohenriquex00@gmail.com>
2023-12-02 15:36:43 -0300

    armazenando dados do usuario no header e roteando para tela principal

--- mkshop_frontend/src/app/components/header/components/user-info-header/user-info-header.component.ts
+++ mkshop_frontend/src/app/components/header/components/user-info-header/user-info-header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user-service.service';
 
 @Component({
   selector: 'app-user-info-header',
   templateUrl: './user-info-header.component.html',
-  styleUrls: ['./user-info-header.component.scss']
+  styleUrls: ['./user-info-header.component.scss'],
 })
-export class UserInfoHeaderComponent {
+export class UserInfoHeaderComponent implements OnInit {
+  user: any;
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.user = this.userService.getUser();
+  }
   logado = true;
 }
