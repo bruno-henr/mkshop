ba1811754636e65efbbb300ed0debccd8995acd2
Bruno Henrique <brunohenriquex00@gmail.com>
2023-12-12 15:10:21 -0300

    criando metodo  para sair da conta

--- mkshop_frontend/src/app/pages/perfil/perfil.component.ts
+++ mkshop_frontend/src/app/pages/perfil/perfil.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { UserService } from 'src/app/services/user/user-service.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.scss'],
+  providers: [ConfirmationService, MessageService],
 })
 export class PerfilComponent implements OnInit {
   user: any;
   userLabel!: string;
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private confirmationService: ConfirmationService,
+  ) {}
 
   ngOnInit(): void {
     this.user = this.userService.getUser();
     this.userLabel = String(this.user.full_name.split(' ')[0][0]).toUpperCase();
+  }
+
+  confirm(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Tem certeza que deseja sair da sua conta?',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: 'Sim',
+      rejectLabel: 'Não',
+      accept: () => {
+        this.userService.signOutUser();
+      },
+    });
   }
 }
--- mkshop_frontend/src/app/services/user/user-service.service.ts
+++ mkshop_frontend/src/app/services/user/user-service.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { Address } from 'src/app/interfaces/Address';
 import { User } from 'src/app/interfaces/User';
@@ -8,13 +9,14 @@ import { User } from 'src/app/interfaces/User';
   providedIn: 'root',
 })
 export class UserService {
-  
-  private token_jwt = JSON.parse(window.localStorage.getItem('token') as string)
-    .token;
+  private token_jwt =
+    window.localStorage.getItem('token') != null
+      ? JSON.parse(window.localStorage.getItem('token') as string).token
+      : null;
 
   private user!: User;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
   apiURL = 'http://localhost:8080';
 
   // Http Options
@@ -30,7 +32,7 @@ export class UserService {
   }
 
   getUser() {
-    return JSON.parse(window.localStorage.getItem('user') as string)
+    return JSON.parse(window.localStorage.getItem('user') as string);
   }
 
   updateTokenJWT(token: string) {
@@ -41,11 +43,9 @@ export class UserService {
   getUserByUsername(username: string) {
     let result = this.http.get(this.apiURL + '/user/username', {
       params: new HttpParams().set('username', username),
-    })
+    });
 
-    
-
-    return result
+    return result;
   }
 
   addAddressToUser(address: Address) {
@@ -54,5 +54,12 @@ export class UserService {
 
   public getUserIfLogged(): Observable<boolean> {
     return of(true);
+  }
+
+  signOutUser() {
+    window.localStorage.removeItem('products');
+    window.localStorage.removeItem('token');
+    window.localStorage.removeItem('user');
+    this.router.navigate(['/login']);
   }
 }
